b6ffd9e8558e0f7d4c46b628e66f8431087ac487
Zoilo Granda <zgranda@gmail.com>
2020-04-20 17:06:16 -0400

    Added edit profile

--- github-profile/src/reducers/reducer_profile.js
+++ github-profile/src/reducers/reducer_profile.js
@@ -1,13 +1,30 @@
-import {PROFILE_FETCHED} from '../actions/actions_profile.js'
-
-const profile = (state ={}, action)=>{
+import { PROFILE_FETCHED, PROFILE_EDITED } from '../actions/actions_profile.js'
+//PROFILE_EDITED NO SE USA, ES UN EJEMPLO
+const profile = (state = {}, action) => {
  switch (action.type) {
   case PROFILE_FETCHED:
    return action.payload
+  case PROFILE_EDITED:
+  let newState = JSON.parse(JSON.stringify(state))
+  newState = replaceProfile(newState, action.payload)
+   return action.payload
   default:
-  return state
-   
+   return state
+
  }
 }
 
 export default profile;
+
+function replaceProfile(profiles, newProfile) {
+ let newProfiles =[]
+ profiles.forEach((thisProfile) => {
+  if (thisProfile.name===newProfile.name) {
+   newProfiles.push(profile)
+  }else {
+   newProfiles.push(thisProfile)
+  }
+  
+ });
+ return newProfiles
+}
